3abaf194bd0b10f6fb96d33c83976d22d8df99be
Julio Efrain Pacheco Sanginez <efrain.pacheco@ibm.com>
2021-04-28 12:14:05 -0500

    Se hicieron las pruebas unitarisa para la clase DAO

--- src/test/java/com/atm/bank/dao/OptionDaoTest.java
+++ src/test/java/com/atm/bank/dao/OptionDaoTest.java
@@ -1,5 +1,65 @@
 package com.atm.bank.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.atm.bank.model.Card;
+
+@RunWith(MockitoJUnitRunner.class)
 public class OptionDaoTest {
+
+  @InjectMocks
+  private OptionDao optionDao;
+
+  @Test
+  public void testCheckBalance() {
+    Card card = generateCard();
+    String previewResponse = "Amount in the account: " + card.getAmount();
+    String response = optionDao.checkBalance(card);
+
+    assertEquals(previewResponse, response);
+  }
+
+  @Test
+  public void testDepositMoney() {
+    Card card = generateCard();
+    String previewResponse = "Operation (deposit) completed successfully."
+        + " Your actual amount is: " + card.getAmount() + ". Don't forget to take your card";
+    String response = optionDao.depositMoney(card, Mockito.anyDouble());
+
+    assertEquals(previewResponse, response);
+  }
+
+  @Test
+  public void testWithdrawMoney() {
+    Card card = generateCard();
+    String previewResponse = "Operation (withdraw) completed successfully."
+        + " Your actual amount is: " + card.getAmount() + ". Don't forget to take your card";
+    String response = optionDao.withdrawMoney(card, Mockito.anyDouble());
+
+    assertEquals(previewResponse, response);
+  }
+
+  @Test
+  public void testTransferMoney() {
+    Card card = generateCard();
+    String previewResponse = "The transaction for the other account has been successful."
+        + " The actual amount in your account is: " + card.getAmount();
+    String response = optionDao.transferMoney(card, Mockito.anyDouble());
+
+    assertEquals(previewResponse, response);
+  }
+
+  private Card generateCard() {
+    Card card = new Card();
+    card.setPin(Mockito.anyInt());
+    card.setAmount(Mockito.anyDouble());
+    return card;
+  }
 
 }
